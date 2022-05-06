d5e23ef431be84d61c359f59c3e0b60ba141a232
jescas85 <63942193+jescas85@users.noreply.github.com>
2022-05-06 00:12:53 -0500

    Update configGeneral.php

--- core/configGeneral.php
+++ core/configGeneral.php
@@ -1,6 +1,6 @@
 <?php
 //define( "SERVER_URL", "http://localhost/mvc/" );
-define('PATH', 'http://' . $_SERVER['HTTP_HOST'] . '/');
+define('PATH', 'http://' . $_SERVER['HTTP_HOST'] . '/GesTicket/');
 define('PATH_IMG', 'http://' . $_SERVER['HTTP_HOST'] . '/views/public/images/');
 define("COMPANY", "GesTicket .::. GRUPO CIELO");
 define("COMPANY_MAIL", "gesticket@grupocielo.com.mx");
